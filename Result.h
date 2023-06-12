b428d63caecd37c4f2644949fee7ee20ae71bcfb
Bruno Cecconi <brunocecc3@gmail.com>
2023-06-12 12:46:38 -0300

    * Replaced std::string_view to simple const char*.

--- Result.h
+++ Result.h
@@ -35,8 +35,8 @@
 		return {ResultInfo{TYPE_NAME, DESCRIPTION, ERROR_MESSAGE}};                                                    \
 	}
 
-#define RESULT_PARAM_OPT Result& Result = *(Result*)&ResultOk
-#define RESULT_PARAM	 Result& Result
+#define RESULT_PARAM_OPT result_t& Result = *(result_t*)&ResultOk
+#define RESULT_PARAM	 result_t& Result
 
 /**
  * @brief Structure that holds data information.
@@ -44,27 +44,18 @@
 */
 struct ResultInfo
 {
-	constexpr ResultInfo(std::string_view Name, std::string_view Description, std::string_view ErrorMessage)
+	constexpr ResultInfo(const char* Name, const char* Description, const char* ErrorMessage)
 		: Name{Name}, Description{Description}, ErrorMessage{ErrorMessage}
 	{
 	}
 
-	const std::string_view Name, Description, ErrorMessage;
+	const char* Name, *Description, *ErrorMessage;
 };
 
-using Result = ResultInfo (*)();
+using result_t = ResultInfo (*)();
 
-constexpr ResultInfo ResultOk()
-{
-	return { "Ok", "Result ok.", "No error." };
-}
-constexpr ResultInfo ResultFail()
-{
-	return { "Fail", "Generic fail.", "Failed. An unknow error has occurred." };
-}
-constexpr ResultInfo ResultNullPtr()
-{
-	return { "NullPtr", "Null pointer.", "Invalid pointer, it's null." };
-}
+RESULT_DEFINE(Ok, "Ok", "Result ok.", "No error.");
+RESULT_DEFINE(Fail, "Fail", "Generic fail.", "Failed. An unknow error has occurred.");
+RESULT_DEFINE(NullPtr, "NullPtr", "Null pointer.", "Invalid pointer, it's null.");
 
 #endif
